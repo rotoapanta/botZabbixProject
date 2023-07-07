ce5f5b3b55fe17f26e8cf3f05e0dfa6c4b02e715
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2023-07-07 15:53:43 -0500

    Update run_bot_zabbix.sh

--- run_bot_zabbix.sh
+++ run_bot_zabbix.sh
@@ -32,5 +32,8 @@ conda activate ${CONDA_ENV}
 # Navega hacia el directorio del script Python
 cd ${SCRIPT_PATH}
 
+# Instala las dependencias del archivo requirements.txt
+pip install -r requirements.txt
+
 # Ejecuta el script Python con el archivo de configuración y redirige la salida estándar a la salida de error
 python ./${PYTHON_SCRIPT} ./${CONFIG_FILE} >&2
