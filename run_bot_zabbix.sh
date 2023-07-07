75c4654ed06a064d2dd5f15d97cdeb3b272f5448
Roberto Toapanta <rtoapanta@igepn.edu.ec>
2023-07-07 15:12:54 -0500

    Modified code

--- run_bot_zabbix.sh
+++ run_bot_zabbix.sh
@@ -3,9 +3,25 @@
 # Define variables
 CONDA_PATH="/home/rotoapanta/anaconda3/bin"
 CONDA_ENV="bot_zabbix_env"
-SCRIPT_PATH="/home/rotoapanta/script/digital_tiltmeter_zabbix"
+SCRIPT_PATH="/home/rotoapanta/script/bot_zabbix"
 PYTHON_SCRIPT="app.py"
 CONFIG_FILE="config.ini"
+
+# Validar la existencia de rutas y archivos
+if [ ! -d "${CONDA_PATH}" ]; then
+    echo "La ruta de Conda no existe: ${CONDA_PATH}"
+    exit 1
+fi
+
+if [ ! -f "${SCRIPT_PATH}/${PYTHON_SCRIPT}" ]; then
+    echo "El script Python no existe: ${SCRIPT_PATH}/${PYTHON_SCRIPT}"
+    exit 1
+fi
+
+if [ ! -f "${SCRIPT_PATH}/${CONFIG_FILE}" ]; then
+    echo "El archivo de configuración no existe: ${SCRIPT_PATH}/${CONFIG_FILE}"
+    exit 1
+fi
 
 # Carga el entorno de Conda dentro de la shell de bash
 eval "$(${CONDA_PATH}/conda shell.bash hook)"
@@ -14,7 +30,6 @@ eval "$(${CONDA_PATH}/conda shell.bash hook)"
 conda activate ${CONDA_ENV}
 
 # Navega hacia el directorio del script Python
-# shellcheck disable=SC2164
 cd ${SCRIPT_PATH}
 
 # Ejecuta el script Python con el archivo de configuración y redirige la salida estándar a la salida de error
